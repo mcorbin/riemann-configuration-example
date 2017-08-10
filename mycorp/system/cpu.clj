a18fcc25d698404baf8d82623ab032d0e1bcdfb1
mcorbin <corbin.math@gmail.com>
2017-08-10 20:48:15 +0200

    Add link to article for cassandra cpu

--- mycorp/system/cpu.clj
+++ mycorp/system/cpu.clj
@@ -7,6 +7,8 @@
             [mycorp.output.email :as email]
             [clojure.tools.logging :refer :all]))
 
+;; cf https://mcorbin.fr/posts/09-08-2017-coalesce/
+
 (def cpu-mean-alert-cassandra
   "A stream checking if the cpu mean for all hosts with service = `cpu` and tagged `cassandra` is > to 60"
   ;; filter by services and tags
